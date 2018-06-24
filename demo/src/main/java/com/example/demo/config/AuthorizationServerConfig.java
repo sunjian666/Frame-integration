0af5ee2ce40dbf6b120dd63555891c1bc4bf03f4
sunjian <1158454917@qq.com>
2018-06-24 11:30:33 +0800

    微调

---
+++ demo/src/main/java/com/example/demo/config/AuthorizationServerConfig.java
@@ -0,0 +1,10 @@
+/*
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+@Configuration
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+}
+*/
